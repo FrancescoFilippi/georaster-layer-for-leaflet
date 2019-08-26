933f0487d4e9535a83ac742d2b7695aee2227c41
Francesco <ffilippi@myti.it>
2019-08-26 10:04:02 +0200

    new build

--- georaster-layer-for-leaflet.bundle.js
+++ georaster-layer-for-leaflet.bundle.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
-
 /* global L, proj4 */
 // const {
 //   isUTM,
@@ -112,11 +110,11 @@ var GeoRasterLayer = L.GridLayer.extend({
         /* source raster doesn't use latitude and longitude,
            so need to reproject point from lat/long to projection of raster
         */
-        var _projector$inverse = _this.projector.inverse([lng, lat]),
-            _projector$inverse2 = _slicedToArray(_projector$inverse, 2),
-            x = _projector$inverse2[0],
-            y = _projector$inverse2[1];
+        // const [x, y] = this.projector.inverse([lng, lat]);
 
+        var inverse = _this.projector.inverse([lng, lat]);
+        var x = inverse.x;
+        var y = inverse.y;
         return {
           y: Math.floor((ymax - y) / _this._pixelHeight),
           x: Math.floor((x - xmin) / _this._pixelWidth)
